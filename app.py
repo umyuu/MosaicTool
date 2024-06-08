c75b97263a01962ef4764c5242a183a6fa7e0d83
umyuu <124dtiaka@gmail.com>
2024-06-08 23:30:20 +0900

    ver 0.0.1

--- app.py
+++ app.py
@@ -3,20 +3,18 @@
     MosaicTool
 """
 from argparse import ArgumentParser
-from dataclasses import dataclass
-from decimal import Decimal, ROUND_UP
-from functools import partial
 from pathlib import Path
 import sys
 import os
 import time
 import tkinter as tk
-from tkinter import filedialog
 
 from tkinterdnd2 import DND_FILES, TkinterDnD
 from PIL import Image, ImageTk
 
-from widgets.core import PhotoImageButton
+from mosaicimagefile import MosaicImageFile, MosaicImage
+from widgets import HeaderFrame, FooterFrame
+
 
 PROGRAM_NAME = 'MosaicTool'
 __version__ = '0.0.1'
@@ -38,60 +36,6 @@ args = parser.parse_args()
 if args.paths is not None:
     for path in args.paths:
         print(f"引数: {path}")
-
-
-@dataclass
-class TargetFile:
-    _file_path: str = ""
-    # ファイルのメタデータを取得
-    file_stat = Path(_file_path).stat()
-
-    @property
-    def mtime(self) -> str:
-        # 最終更新日時を取得
-        timestamp = self.file_stat.st_mtime
-        # タイムスタンプをISO 8601形式に変換
-        return time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(timestamp))
-
-    @property
-    def st_size(self) -> int:
-        return self.file_stat.st_size
-
-
-class HeaderFrame(tk.Frame):
-    def __init__(self, master, bg):
-        super().__init__(master, bg=bg)
-        self.createWidgets()
-
-    def createWidgets(self):
-        self.btn_select_file = PhotoImageButton(self, image_path=str(Path(icons_path, "file_open_24dp_FILL0_wght400_GRAD0_opsz24.png")), command=partial(self.on_select_file, event=None))
-        self.btn_select_file.grid(row=0, column=0, padx=(0, 0))
-        self.btn_back_file = PhotoImageButton(self, image_path=str(Path(icons_path, "arrow_back_24dp_FILL0_wght400_GRAD0_opsz24.png")), command=partial(self.on_select_file, event=None))
-        self.btn_back_file.grid(row=0, column=1, padx=(4, 0))
-        self.btn_forward_file = PhotoImageButton(self, image_path=str(Path(icons_path, "arrow_forward_24dp_FILL0_wght400_GRAD0_opsz24.png")), command=partial(self.on_select_file, event=None))
-        self.btn_forward_file.grid(row=0, column=2, padx=(4, 0))
-        self.widgetHeader = tk.Label(self, text="ヘッダーはここ", font=("", 10))
-        self.widgetHeader.grid(row=0, column=3, padx=(4, 0))
-
-    def on_select_file(self, event):
-        # 画像形式
-        ImageFormat = {
-            'PNG': ('*.png', ),
-            'JPEG': ('*.jpg', '*.jpeg', ),
-            'WEBP': ('*.webp', ),
-            'BMP': ('*.bmp', ),
-            'PNM': ('*.pbm', '*.pgm', '*.ppm', )
-        }
-        IMAGE_FILE_TYPES = [
-            ('Image Files', ImageFormat['PNG'] + ImageFormat['JPEG'] + ImageFormat['WEBP'] + ImageFormat['BMP']),
-            ('png (*.png)', ImageFormat['PNG']),
-            ('jpg (*.jpg, *.jpeg)', ImageFormat['JPEG']),
-            ('webp (*.webp)', ImageFormat['WEBP']),
-            ('bmp (*.bmp)', ImageFormat['BMP']),
-            ('*', '*.*')
-        ]
-        print('on_select_file...')
-        return filedialog.askopenfilename(parent=self, filetypes=IMAGE_FILE_TYPES)
 
 
 class MainFrame(tk.Frame):
@@ -122,27 +66,48 @@ class MainFrame(tk.Frame):
 
         # ドラッグ終了時のイベントをバインド
         self.canvas.bind("<ButtonRelease-1>", self.end_drag)
-        self.canvas.image = None
         self.photo = None
         self.updateImage("")
+        self.t = MosaicImageFile("")
 
     def updateImage(self, filepath):
         if len(filepath) == 0:
             return
-        
-        self.canvas.image = Image.open(filepath)
-        self.photo = ImageTk.PhotoImage(self.canvas.image)
+        self.t = MosaicImageFile(filepath)
+
+        #self.canvas.image = Image.open(filepath)
+        #self.photo = ImageTk.PhotoImage(self.canvas.image)
+
+        self.original_image = Image.open(filepath)  # 元の画像を開く
+        self.photo = ImageTk.PhotoImage(self.original_image)  # 元の画像のコピーをキャンバスに表示
 
         # 画像を更新
         self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-
         # キャンバスのスクロール領域を設定
-        self.canvas.config(scrollregion=(0, 0, self.canvas.image.width, self.canvas.image.height))
+        self.canvas.config(scrollregion=(0, 0, self.original_image.width, self.original_image.height))
+
+    def save_canvas_image(self, filename):
+        if (len(filename) == 0):
+            return
+        # キャンバスの位置とサイズを取得
+        #x = 0
+        #y = 0
+        #x1 = x + self.canvas.image.width
+        #y1 = y + self.canvas.image.height
+
+        #x = self.canvas.winfo_rootx()
+        #y = self.canvas.winfo_rooty()
+        #x1 = x + self.canvas.winfo_width()
+        #y1 = y + self.canvas.winfo_height()
+
+        # キャンバスの内容をキャプチャ
+        self.original_image.save(filename)
+        #ImageGrab.grab().crop((x, y, x1, y1)).save(filename)
 
     def start_drag(self, event):
-        # ドラッグ開始位置を記録
-        self.start_x = event.x
-        self.start_y = event.y
+        # ドラッグ開始位置を記録（キャンバス上の座標に変換）
+        self.start_x = int(self.canvas.canvasx(event.x))
+        self.start_y = int(self.canvas.canvasy(event.y))
 
     def dragging(self, event):
         if self.photo is None:
@@ -152,9 +117,9 @@ class MainFrame(tk.Frame):
         self.canvas.create_rectangle(self.start_x, self.start_y, event.x, event.y, outline='red', tags='dragging')
 
     def end_drag(self, event):
-        # ドラッグ終了位置を取得
-        end_x = event.x
-        end_y = event.y
+        # ドラッグ終了位置を取得（キャンバス上の座標に変換）
+        end_x = int(self.canvas.canvasx(event.x))
+        end_y = int(self.canvas.canvasy(event.y))
 
         # 選択領域にモザイクをかける
         self.apply_mosaic(self.start_x, self.start_y, end_x, end_y)
@@ -163,62 +128,26 @@ class MainFrame(tk.Frame):
         self.canvas.delete('dragging')
 
     def apply_mosaic(self, start_x, start_y, end_x, end_y):
-        if self.canvas.image is None:
+        if self.photo is None:
             return
 
-        # モザイクをかける領域を切り出す
-        region = self.canvas.image.crop((start_x, start_y, end_x, end_y))
-
-        # 切り出した領域を縮小し、元のサイズに拡大することでモザイクをかける
-        region = region.resize((10, 10), Image.BOX).resize(region.size, Image.NEAREST)
-
-        # モザイクをかけた領域を元の画像に戻す
-        self.canvas.image.paste(region, (start_x, start_y, end_x, end_y))
-
-        # 画像を更新
-        self.photo = ImageTk.PhotoImage(self.canvas.image)
+        mosaic = MosaicImage(self.original_image)
+        mosaic.apply_mosaic(start_x, start_y, end_x, end_y)
+        self.original_image = mosaic.get_image()
+        self.photo = ImageTk.PhotoImage(self.original_image)  # 元の画像のコピーをキャンバスに表示
+        # キャンバスの画像も更新
+        #self.photo.paste(region, (start_x, start_y, end_x, end_y))
         self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-
+        new_filepath = self.t.saveMosaic()
+        self.save_canvas_image(str(new_filepath))
         # キャンバスのスクロール領域を設定
-        self.canvas.config(scrollregion=(0, 0, self.canvas.image.width, self.canvas.image.height))
-
-
-def round_up_decimal(value: Decimal):
-    # 小数点以下2桁に切り上げ
-    rounded_value = value.quantize(Decimal('0.01'), rounding=ROUND_UP)
-    return rounded_value
-
-
-class FooterFrame(tk.Frame):
-    def __init__(self, master, bg):
-        super().__init__(master, bg=bg)
-        self.createWidgets()
-
-    def createWidgets(self):
-        self.modified = tk.Label(self, text=" " * 20, bd=1, relief=tk.SUNKEN, anchor=tk.W)
-        self.modified.grid(row=0, column=0, sticky=tk.W + tk.E)
-        self.fileSizeBar = tk.Label(self, text=" " * 20, bd=1, relief=tk.SUNKEN, anchor=tk.W)  # ファイルサイズ表示用のラベルを追加
-        self.fileSizeBar.grid(row=0, column=1, sticky=tk.W + tk.E)
-        self.paddingLabel = tk.Label(self, text="フッターはここ")  # 余白調整用のラベルを追加
-        self.paddingLabel.grid(row=0, column=2, sticky=tk.W + tk.E)  # stickyをW+Eに変更
-        self.columnconfigure(0, weight=0)
-        self.columnconfigure(1, weight=0)
-        self.columnconfigure(2, weight=1)  # 列2（余白調整用のラベル）にweightを設定
-
-    def updateStatus(self, filepath):
-        target = TargetFile(filepath)
-        # ステータスバーに表示
-        self.modified.config(text=target.mtime)
-        # ファイルサイズを取得
-        filesize_kb = Decimal(target.st_size) / 1024
-        # ファイルサイズを表示
-        self.fileSizeBar.config(text=str(round_up_decimal(filesize_kb)) + " KB")
+        self.canvas.config(scrollregion=(0, 0, self.original_image.width, self.original_image.height))
 
 
 class MainPage(tk.Frame):
     def __init__(self):
         super().__init__(bg="#00C8B4")
-        self.HeaderFrame = HeaderFrame(self, bg="#44F7D3")
+        self.HeaderFrame = HeaderFrame(self, "#44F7D3", icons_path)
         self.HeaderFrame.grid(column=0, row=0, sticky=(tk.E + tk.W + tk.S + tk.N))
         self.MainFrame = MainFrame(self, bg="#88FFEB")
         self.MainFrame.grid(column=0, row=1, sticky=(tk.E + tk.W + tk.S + tk.N))
@@ -235,15 +164,7 @@ class MainPage(tk.Frame):
         self.columnconfigure(0, weight=1)  # ヘッダーをウィンドウ幅まで拡張する
 
     def onUpdate(self, e):
-        message = '\n' + e.data
         self.MainFrame.updateImage(e.data)
-        #text = self.MainFrame.textbox
-        #text.configure(state='normal')
-        #text.insert(tk.END, message)
-        #text.configure(state='disabled')
-
-        #text.see(tk.END)
-
         # フッターのステータスバーを更新
         self.FooterFrame.updateStatus(e.data)
 
@@ -258,7 +179,7 @@ class MyApp(TkinterDnD.Tk):
         self.minsize(width, height)
         self.set_window_title("")  # プログラム名とバージョン番号を表示
 
-        self.TargetFile = TargetFile()
+        self.TargetFile = MosaicImageFile()
         self.MainPage = MainPage()
         # ドラッグアンドドロップ
         self.MainPage.drop_target_register(DND_FILES)
@@ -279,9 +200,6 @@ class MyApp(TkinterDnD.Tk):
         """
         self.set_window_title(e.data)
         self.MainPage.onUpdate(e)
-
-        image = Image.open(e.data)
-        #image.show()
 
 
 if __name__ == "__main__":
