78ec2babac5794093817aeb70585eda40cdd071a
umyuu <124dtiaka@gmail.com>
2024-06-09 02:27:31 +0900

    フッターに画像サイズを表示します。

--- widgets.py
+++ widgets.py
@@ -8,10 +8,10 @@ import tkinter as tk
 from tkinter import filedialog
 from pathlib import Path
 
+from PIL import Image
+
 from lib.models import MosaicImageFile
 from lib.utils import round_up_decimal
-
-
 
 
 class PhotoImageButton(tk.Button):
@@ -69,24 +69,39 @@ class HeaderFrame(tk.Frame):
 
 
 class FooterFrame(tk.Frame):
+    """
+    画面のヘッダー部
+    """
     def __init__(self, master, bg):
         super().__init__(master, bg=bg)
         self.createWidgets()
 
     def createWidgets(self):
+        self.imageSizeBar = tk.Label(self, text=" " * 20, bd=1, relief=tk.SUNKEN, anchor=tk.W)  # 画像サイズ表示用のラベルを追加
+        self.imageSizeBar.grid(row=0, column=0, sticky=tk.W + tk.E)
         self.modified = tk.Label(self, text=" " * 20, bd=1, relief=tk.SUNKEN, anchor=tk.W)
-        self.modified.grid(row=0, column=0, sticky=tk.W + tk.E)
+        self.modified.grid(row=0, column=1, sticky=tk.W + tk.E)
         self.fileSizeBar = tk.Label(self, text=" " * 20, bd=1, relief=tk.SUNKEN, anchor=tk.W)  # ファイルサイズ表示用のラベルを追加
-        self.fileSizeBar.grid(row=0, column=1, sticky=tk.W + tk.E)
+        self.fileSizeBar.grid(row=0, column=2, sticky=tk.W + tk.E)
+
         self.paddingLabel = tk.Label(self, text="フッターはここ")  # 余白調整用のラベルを追加
-        self.paddingLabel.grid(row=0, column=2, sticky=tk.W + tk.E)  # stickyをW+Eに変更
+        self.paddingLabel.grid(row=0, column=3, sticky=tk.W + tk.E)
+
         self.columnconfigure(0, weight=0)
         self.columnconfigure(1, weight=0)
-        self.columnconfigure(2, weight=1)  # 列2（余白調整用のラベル）にweightを設定
+        self.columnconfigure(2, weight=0)
+        self.columnconfigure(3, weight=1)  # 列2（余白調整用のラベル）にweightを設定
 
     def updateStatus(self, filepath):
         target = MosaicImageFile(filepath)
-        # ステータスバーに表示
+
+        # 画像の幅と高さを取得
+        with Image.open(filepath) as img:
+            width, height = img.size
+
+        # 画像の幅と高さを表示
+        self.imageSizeBar.config(text=f"Width: {width}px, Height: {height}px")
+        # 最終更新日時
         self.modified.config(text=target.mtime)
         # ファイルサイズを取得
         filesize_kb = Decimal(target.st_size) / 1024
