9ff9fbf367111eec185b4ff74dee756aec03ef4a
umyuu <124dtiaka@gmail.com>
2024-06-12 07:39:59 +0900

    単体テスト実行時にエラーが発生する不具合対応
    self.tk.call(
    _tkinter.TclError: image "pyimage7" doesn't exist

--- test/test_controllers.py
+++ test/test_controllers.py
@@ -2,14 +2,15 @@ from dataclasses import dataclass
 import sys
 import os
 import unittest
+from unittest.mock import Mock
 from pathlib import Path
-import tkinter
-import tkinterdnd2
+
 # プロジェクトのルートディレクトリをシステムパスに追加
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.utils import Stopwatch
-import app
+from src.controllers import AppController
+from src.models import DataModel
+from src.widgets import MainPage
 
 
 class TestAppController(unittest.TestCase):
@@ -30,17 +31,19 @@ class TestAppController(unittest.TestCase):
         class Event:
             data: str
 
-        my_app = app.MyApp()
-        my_app.after_launch([])
+        model = DataModel()
+        main_page = Mock(MainPage)
 
+        controller = AppController(model, main_page, self.dummy_func)
         event = Event(' '.join(assets))
 
-        controller = my_app.controller
         controller.handle_drop(event)
         count = controller.model.count()
 
         self.assertTrue(count == 3, f"test_drop_file_parser error {count}")
 
+    def dummy_func(self, text: str):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
