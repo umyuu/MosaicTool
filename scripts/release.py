87156fd52a82320f001638880769b2330a78ed66
umyuu <124dtiaka@gmail.com>
2024-06-16 12:20:43 +0900

    配布用のzipにサンプルの設定ファイルを同封します。

--- scripts/release.py
+++ scripts/release.py
@@ -6,15 +6,17 @@ from dataclasses import dataclass, field
 from datetime import datetime
 import glob
 import hashlib
+import os
 from pathlib import Path
+import sys
 import time
 import zipfile
-import sys
-import os
+
 # プロジェクトのルートディレクトリをシステムパスに追加
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.utils import get_package_version
+from src import PROGRAM_NAME, get_package_version
+
 __version__ = get_package_version()
 
 
@@ -55,7 +57,7 @@ def create_readMe(file1_path: Path, file2_path: Path, hash_value: str, output_pa
     readme_content += file1_path.read_text("utf-8")
     readme_content += "\n\n"
     readme_content += "## ハッシュ値  \n"
-    readme_content += f"MosaicTool.exe Hash Value (SHA-256): {hash_value}\n"
+    readme_content += f"{PROGRAM_NAME}.exe Hash Value (SHA-256): {hash_value}\n"
     readme_content += file2_path.read_text("utf-8")
 
     output_path.write_text(readme_content, "utf-8")
@@ -79,22 +81,27 @@ def get_compressible_files(root_dir: Path, exclude_files: list[Path]) -> list[Pa
     return compressible_files
 
 
-def create_zip(source_files: list[Path], output_zip: Path) -> int:
+def create_zip(source_files: list[Path], output_zip: Path, additional_files_with_names: dict[Path, str]) -> int:
     """
     引数で指定されたファイルをZIPファイルに格納します。
 
     :param source_files: ZIPファイルに含めるファイル。
     :param output_zip: 出力するZIPファイルのパス。
+    :param additional_files_with_names: 追加ファイルとその保存名の辞書。
     :return: ZIPファイルに追加されたファイルの数。
     """
     count: int = 0
 
-    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zip_file:
         for file_path in source_files:
             print(file_path)
-            zipf.write(file_path, arcname=file_path.name)
+            zip_file.write(file_path, arcname=file_path.name)
             count += 1
 
+        for file_path, save_name in additional_files_with_names.items():
+            print(file_path)
+            zip_file.write(file_path, arcname=save_name)
+            count += 1
     return count
 
 
@@ -104,33 +111,34 @@ class SetupConfigRation:
     セットアップ設定
     """
     source_dir: Path = Path("dist")
-    output_zip: Path = source_dir / "MosaicTool.zip"
-    app_file: Path = source_dir / "MosaicTool.exe"
+    output_zip: Path = source_dir / f"{PROGRAM_NAME}.zip"
+    app_file: Path = source_dir / f"{PROGRAM_NAME}.exe"
     handouts_file: Path = source_dir / "handouts.txt"
     exclude_files: list[Path] = field(default_factory=list)
-    additional_files: list[Path] = field(default_factory=list)
 
 
 def main():
     start: float = time.perf_counter()
     # Configuration
     config = SetupConfigRation()
+    # 除外するファイル
     config.exclude_files.extend([config.output_zip, config.source_dir / ".gitignore", config.handouts_file])
-    config.additional_files.extend([
-        Path("docs/initial_screen.png"),
-        Path("MosaicTool.json"),
-    ])
+    # zipファイルに追加するファイル
 
-    # List of additional files to include in the zip
+    additional_files_with_names = {
+        Path("docs/initial_screen.png"): "initial_screen.png",
+        Path(f"{PROGRAM_NAME}.json"): f"sample/{PROGRAM_NAME}.json",
+    }
+
     hash_value = hash_compute(config.app_file)
     print(f"Compute hash, ({time.perf_counter() - start:.3f}s)")
 
     create_readMe(Path("ReadMe.md"), config.handouts_file, hash_value, config.source_dir / "ReadMe.txt")
     print(f"Created ReadMe.txt, ({time.perf_counter() - start:.3f}s)")
 
-    compressible_files = get_compressible_files(config.source_dir, config.exclude_files) + config.additional_files
+    compressible_files = get_compressible_files(config.source_dir, config.exclude_files)
     print(f"Get_compressible_files, ({time.perf_counter() - start:.3f}s)")
-    count: int = create_zip(compressible_files, config.output_zip)
+    count: int = create_zip(compressible_files, config.output_zip, additional_files_with_names)
 
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"Release zip file created at: {config.output_zip} on {current_time}")
